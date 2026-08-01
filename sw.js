51a6e184e1738aa0cfd74f1f1119e614c0a0b664
风雨彩虹 <492600406@qq.com>
2026-08-01 17:24:34 +0800

    add PWA support for home screen install

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'exercise-checkin-v1';
+const ASSETS = [
+  '/lehuo-exercise/',
+  '/lehuo-exercise/index.html',
+  '/lehuo-exercise/manifest.json',
+  '/lehuo-exercise/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
